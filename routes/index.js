efd264bc29944d18a44c3678efb9c90e41f70c14
Luca Cornaglia <lcornag@gmail.com>
2018-03-23 14:34:08 +0100

    carousel a cada destino onclick -alpha-

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mainPage', {
+      res.render('mainPage', {
       title: 'Geekshubs Travels',
       ciudades: [
-          {id:1, name:'finland', precio:399},
-          {id:2, name:'marruecos', precio:670},
-          {id:3, name:'nicaragua', precio:520},
-          {id:4, name:'peru', precio:800},
-          {id:5, name:'singapur', precio:899},
-          {id:6, name:'suiza', precio:320}
-      ],
+          {id:1, name:'Finland', precio:399},
+          {id:2, name:'Marruecos', precio:670},
+          {id:3, name:'Nicaragua', precio:520},
+          {id:4, name:'Peru', precio:800},
+          {id:5, name:'Singapur', precio:899},
+          {id:6, name:'Suiza', precio:320}
+      ]
   });
+});
+router.get('/destino',function(req,res,next){
+    res.render('destino');
 });
 
 module.exports = router;
